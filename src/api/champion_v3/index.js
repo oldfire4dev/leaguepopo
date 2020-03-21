1247102e1f3ea0dad435a55d88ec075e6eb2aabe
Lucas Matheus <lucasmatheus2021@gmail.com>
2020-03-21 01:18:19 -0300

    added champion v3 api
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/api/champion_v3/index.js
+++ src/api/champion_v3/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+//import React from 'react';
 const kayn = require('./../setting');
 
 const url = 'https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=';
 
-export default class Champion {
-  getChampionFreeWeek = () => {
-    // kayn.Champion.Rotation.list()
-    // .then(res=>{
-    //   console.log(res)
-    // })
-    // .catch(err => { console.log(err) })
-  }
+
+getChampionFreeWeek = (req, res) => {
+
+      kayn.kaynObject.Champion.Rotation.list()
+      .region(kayn.regions.BRAZIL)
+      .then(Champion => {
+          let ids = Champion.freeChampionIds;
+          return res.send({ id: ids })
+      }) .catch (err => {
+            console.log(err)
+      })
+
+}
+
+listChampions =  (req, res) => {
+      kayn.kaynObject.DDragon.Champion.list()
+      .region(kayn.regions.BRAZIL)
+      .then(Champion => {
+        let data = Champion.data;
+        return res.send({ id: data })
+        }) .catch (err => {
+              console.log(err)
+        })
+
+}
+
+module.exports = {
+  getChampionFreeWeek,
+  listChampions
 }
--- src/api/setting.js
+++ src/api/setting.js
@@ -1,29 +1,31 @@
 const { Kayn, REGIONS } = require('kayn');
 
+module.exports = {
+    kaynObject: Kayn('RGAPI-719ced7a-6219-486f-aa93-acc42c4380ec')({
+      region: REGIONS.BRAZIL,
+      apiURLPrefix: 'https://%s.api.riotgames.com',
+      locale: 'pt_BR',
+      debugOptions: {
+          isEnabled: true,
+          showKey: false,
+      },
+      requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: false,
+        shouldExitOn403: false,
+      },
+      cacheOptions: {
+        cache: null,
+        timeToLives: {
+            useDefault: false,
+            byGroup: {},
+            byMethod: {},
+        },
+      },
+    }),
 
-const kayn = Kayn('RGAPI-aab8b0a9-b0f3-4f51-a83f-b855f5c7fcc8')({
-  region: REGIONS.BRAZIL,
-  apiURLPrefix: 'https://%s.api.riotgames.com',
-  locale: 'pt_BR',
-  debugOptions: {
-      isEnabled: true,
-      showKey: false,
-  },
-  requestOptions: {
-    shouldRetry: true,
-    numberOfRetriesBeforeAbort: 3,
-    delayBeforeRetry: 1000,
-    burst: false,
-    shouldExitOn403: false,
-  },
-  cacheOptions: {
-    cache: null,
-    timeToLives: {
-        useDefault: false,
-        byGroup: {},
-        byMethod: {},
-    },
-  },
-})
-
-export default kayn;
+    regions: REGIONS
+    
+};
