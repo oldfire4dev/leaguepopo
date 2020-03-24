3e1940882e83bcd9b684e84c24756b2060f4d02b
lok <cleiton_biou@hotmail.com>
2020-03-24 02:08:06 -0300

    fixes
    Signed-off-by: lok <cleiton_biou@hotmail.com>

--- src/api/setting.js
+++ src/api/setting.js
@@ -1,7 +1,7 @@
 const { Kayn, REGIONS } = require('kayn');
 
 module.exports = {
-    kaynObject: Kayn("RGAPI-f84120c7-9c8f-475d-890c-f0692fd50849")({
+    kaynObject: Kayn('RGAPI-f84120c7-9c8f-475d-890c-f0692fd50849')({
       region: REGIONS.BRAZIL,
       apiURLPrefix: 'https://%s.api.riotgames.com',
       locale: 'pt_BR',
